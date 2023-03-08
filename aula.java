cf05d302aeb76f9107ddf6dfb3872a8104273979
Gabriel-Shallon <shallonhatake@gmail.com>
2023-03-08 16:22:02 -0400

    Create aula.java

---
+++ aula.java
@@ -0,0 +1,30 @@
+private int decimal;
+private String binario, octal ,hexadecimal;
+
+
+public class Conversor(int decimal){
+  decimal = decimal;
+  binario = converterBinario();
+  octal = coneverterOctal();
+  hexadecimal = converterHexadecimal();
+}
+
+
+private String converterBinario(){
+    int temp = decimal;
+    String binario = "";
+    while (temp > 0){
+        int resto
+
+
+    }
+return binario;
+
+}
+
+
+public getBinario(){
+    return binario;
+}
+
+}
