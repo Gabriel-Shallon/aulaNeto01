ba77a25b4c7cb448be006dda73be00a470ad0dd1
Gabriel-Shallon <shallonhatake@gmail.com>
2023-03-08 16:36:14 -0400

    Create PessoaFisica.java

---
+++ aula02/aula02/PessoaFisica.java
@@ -0,0 +1,14 @@
+package aula02;
+
+public class PessoaFisica {
+    private long cpf;
+
+    public long getCpf(){
+    return cpf; 
+    }
+
+    public void setCpf(long cpf){
+        this.cpf = cpf;
+    }
+
+}
