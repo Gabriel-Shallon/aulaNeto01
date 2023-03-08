4cbf46ccc600e99f6676315a6d2fc3da3f7e706d
Gabriel-Shallon <shallonhatake@gmail.com>
2023-03-08 16:22:06 -0400

    Create App.java

---
+++ aula02/aula02/App.java
@@ -0,0 +1,23 @@
+package aula02;
+
+import javax.swing.JOptionPane;
+
+public class App {
+    
+public static void main( String[] args){
+    Pessoa pessoa1;
+    pessoa1 = new Pessoa();
+
+
+
+
+    pessoa1.setNome("Neto");
+    JOptionPane.showMessageDialog(null, pessoa1.getNome()); 
+
+
+}
+
+
+
+
+}
